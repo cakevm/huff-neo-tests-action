b6a543d75544d79ddb009b670b3bb93a8cc23fcd
cakevm <cakevm@proton.me>
2025-01-11 17:32:18 +0100

    Improve script

--- hufftests.sh
+++ hufftests.sh
@@ -4,14 +4,14 @@ location=${1:-"src"}
 extension=${2:-"*.t.huff"}
 format=${3:-"list"}
 
-files=`find $location -type f -name "$extension"`
+files=$(find "$location" -type f -name "$extension")
 
-set -- junk $files
+set -- junk "$files"
 shift
 for word; do
-   hnc $word test --format $format
+   hnc "$word" test --format "$format"
 done
 
 echo "Tests ran on"
-printf '%s\n' "${files[@]}"
+printf '%s\n' "${files[@]}" || printf '%s\n' "${files}"
 
